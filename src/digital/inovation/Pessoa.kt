eaed73242a926b48ddc01959261b6c8a1e355a00
kauane silva <kauane.silva.pereira@everis.com>
2021-03-12 17:17:04 -0300

    second commit

--- src/digital/inovation/Pessoa.kt
+++ src/digital/inovation/Pessoa.kt
@@ -3,7 +3,11 @@ package digital.inovation
 class Pessoa {
     var nome:String = "Kauane"
     var cpf:String = "06223392141"
-}
+    private set
+
+
+    }
+
 
 fun main(){
     val nome = Pessoa()
